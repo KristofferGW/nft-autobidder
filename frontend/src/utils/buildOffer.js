a78e1910576273945fa8ccdd5b771dc2c83e3687
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2024-01-10 13:24:18 +0700

    Added several new functions

--- frontend/src/utils/buildOffer.js
+++ frontend/src/utils/buildOffer.js
@@ -1,5 +1,5 @@
 const { Chain, Fees } = require('opensea-js/lib/types');
-const { apiClient, sdkClients } = require('./apiClient');
+const { apiClient, sdkClient } = require('./apiClient');
 const { getNetwork } = require('./network');
 const { seaportContractAddress } = require('./seaport');
 const { getWallet } = require('./wallet');
@@ -27,7 +27,7 @@ const getOffer = (priceWei) => {
 }
 
 const getFee = (priceWei, feeBasisPoints, receipient) => {
-    const fee = (priceWei * feeBasisPoints) / 1000;
+    const fee = (BigInt(priceWei) * BigInt(feeBasisPoints)) / 1000n;
     if (fee <= 0) {
         return null;
     }
@@ -41,18 +41,77 @@ const getFee = (priceWei, feeBasisPoints, receipient) => {
     }
 }
 
-//Fix function below
 function extractFeesApi(feesObject, priceWei) {
     const fees = [];
 
-    for (const [category, categoryFees] of Object.entries(feesObject)) {
-        for (const [address, basisPoints] of Object.entries(categoryFees)) {
-            const fee = getFee(priceWei, BigInt(basisPoints), address);
-            if (fee) {
-                fees.push(fee);
+    for (const category in feesObject) {
+        if (feesObject.hasOwnProperty(category)) {
+            const categoryFees = feesObject[category];
+
+            for (const address in categoryFees) {
+                if (categoryFees.hasOwnProperty(address)) {
+                    const basisPoints = categoryFees[address];
+
+                    const fee = getFee(priceWei, BigInt(basisPoints), address);
+
+                    if (fee) {
+                        fees.push(fee);
+                    }
+                }
             }
         }
     }
-
     return fees;
 }
+
+const extractFeesSdk = (feesObject, priceWei) => {
+    const fees = []
+    const feesToAdd = [...feesObject.openseaFees, ...feesObject.sellerFees]
+  
+    for (const [address, basisPoints] of feesToAdd) {
+      const fee = getFee(priceWei, BigInt(basisPoints), address)
+      if (fee) {
+        fees.push(fee)
+      }
+    }
+  
+    return fees
+}
+
+// Unsure if getItemFees is needed. Debug if it is indeed needed.
+const getItemFees = async (assetContractAddress, tokenId, priceWei) => {
+    const response = await sdkClient.api.getNFT(
+      Chain.Goerli,
+      assetContractAddress,
+      tokenId,
+    )
+  
+    const collectionSlug = response.nft.collection
+    const collection = await sdkClient.api.getCollection(collectionSlug)
+    const fees = collection.fees
+    return extractFeesSdk(fees, priceWei)
+}
+
+const getCriteriaFees = async (collectionSlug, priceWei) => {
+    const response = await apiClient.get(`v1/collection/${collectionSlug}`)
+  
+    const feesObject = response.data.collection.fees
+    return extractFeesApi(feesObject, priceWei)
+}
+
+const getBuildData = async (collectionSlug, quantity) => {
+    const offerer = getOfferer()
+    const response = await apiClient.post("v2/offers/build", {
+      offerer,
+      quantity,
+      criteria: {
+        collection: {
+          slug: collectionSlug,
+        },
+      },
+      protocol_address: seaportContractAddress,
+    })
+  
+    return response.data.partialParameters
+}
+
