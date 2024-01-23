89d6b07df898dd5e29de6603c58afe845c2f4149
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2024-01-23 13:16:45 +0100

    removed unneccesary code

--- node-app/app.js
+++ node-app/app.js
@@ -6,38 +6,6 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
-// const fetchData = async (maxBid, minDifference) => {
-//   try {
-//     sdk.auth('9d2673aea38642219bf60ddfd03e726a');
-//     sdk.server('https://api.opensea.io');
-
-//     const floorData = await sdk.get_best_listings_on_collection_v2({ limit: '1', collection_slug: 'insrt-finance' });
-
-//     const topBidData = await sdk.get_collection_offers_v2({ collection_slug: 'insrt-finance' });
-
-//     const combinedData = {
-//       floor: floorData.data.listings[0]?.price,
-//       topBid: topBidData.data.offers[0]?.price
-//     };
-
-//     const userMaxBid = parseFloat(maxBid);
-//     const floorPrice = combinedData.floor;
-//     const topBid = combinedData.topBid;
-//     const difference = floorPrice - topBid;
-
-//     console.log(
-//       'Combined data:', combinedData,
-//       'User max bid', userMaxBid,
-//       'Floor price', floorPrice,
-//       'differece', difference
-//       );
-
-//     setInterval(fetchData, 15 * 60 * 1000);
-//   } catch (error) {
-//     console.error('Error combining data', error);
-//   }
-// };
-
 const fetchData = async (maxBid, minDifference) => {
   try {
     sdk.auth('9d2673aea38642219bf60ddfd03e726a');
